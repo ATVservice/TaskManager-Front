f467f0ae9fb038a56a6cb41ccb5203161cbbc195
Bat-sheva Birenbaum <b0556772946@gmail.com>
2025-09-16 11:50:12 +0300

    try

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = process.env.REACT_APP_BACKEND_URL;
+const API_URL = 'https://taskmanager-back-production.up.railway.app'
 
 
 export const loginUser = async (username, password) => {
